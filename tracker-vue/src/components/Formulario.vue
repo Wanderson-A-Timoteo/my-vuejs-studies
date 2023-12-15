f5d9d57e6420295cc5d10b31ea262ae58cd881e3
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-12-15 11:31:26 -0400

    add iniciado criação de metodos para click do botão

--- tracker-vue/src/components/Formulario.vue
+++ tracker-vue/src/components/Formulario.vue
@@ -5,22 +5,24 @@
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
       </div>
       <div class="column">
-        <section>
-          <strong>00:00:00</strong>
-        </section>
-        <button class="button">
-          <span class="icon">
-            <i class="fas fa-play"></i>
-          </span>
-          <span>pay</span>
-        </button>
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>pay</span>
+          </button>
 
-        <button class="button">
-          <span class="icon">
-            <i class="fas fa-stop"></i>
-          </span>
-          <span>stop</span>
-        </button>
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -30,6 +32,14 @@
   import { defineComponent } from 'vue';
   
   export default defineComponent({
-    name: 'Formulario'
-  })
+    name: 'Formulario',
+    methods: {
+      iniciar() {
+        console.log("iniciando");
+      },
+      finalizar() {
+        console.log("finalizando");
+      }
+    }
+  });
 </script>
