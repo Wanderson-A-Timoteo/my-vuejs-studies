f3de554ceb4a0b8f9d7548f97bb2ba1f552e02e7
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-12-15 16:52:02 -0400

    Add evento emits ao salvar tarefa

--- tracker-vue/src/components/Formulario.vue
+++ tracker-vue/src/components/Formulario.vue
@@ -18,6 +18,7 @@
   export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Formulario',
+    emits: ['aoSalvarTarefa'],
     components: {
       Temporizador,
     },
@@ -28,8 +29,10 @@
     },
     methods: {
       finalizarTarefa(tempoDecorrido: number) : void {
-        console.log('tempo da tarefa', tempoDecorrido)
-        console.log('descrição da tarefa', this.descricao)
+        this.$emit('aoSalvarTarefa', {
+          duracaoEmSegundos: tempoDecorrido,
+          descricao: this.descricao
+        })
         this.descricao = '';
       }
     }
