0f346651cb6fbe429e039f7d9c345554a77369e2
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-12-15 14:05:30 -0400

    Add componente Temporizador

---
+++ tracker-vue/src/components/Temporizador.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegungos="tempoEmSegundos"/>
+    <button class="button" @click="iniciar">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>pay</span>
+    </button>
+
+    <button class="button" @click="finalizar">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import Cronometro from './Cronometro.vue';
+  
+  export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Formulario',
+    components: {
+      Cronometro
+    },
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        };
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000);
+        },
+        finalizar() {
+            clearInterval(this.cronometro);
+        }
+    },
+});
+</script>
