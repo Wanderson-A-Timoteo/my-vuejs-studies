c734bc4a36e8b2881b82f2b9343d1b5790943e2c
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-12-15 11:06:49 -0400

    add componente Formulario

--- tracker-vue/src/components/BarraLateral.vue
+++ tracker-vue/src/components/BarraLateral.vue
@@ -11,7 +11,7 @@
   
   export default defineComponent({
     name: 'BarraLateral'
-})
+  })
 </script>
 
 <style scoped>
---
+++ tracker-vue/src/components/Formulario.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="columns is-8" role="form" aria-label="Formulário para criaçãode uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+      </div>
+      <div class="column">
+        <section>
+          <strong>00:00:00</strong>
+        </section>
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-play"></i>
+          </span>
+          <span>pay</span>
+        </button>
+
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-stop"></i>
+          </span>
+          <span>stop</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Formulario'
+  })
+</script>
